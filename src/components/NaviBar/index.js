c14e638b57221d6d61e4a79d3038cd4f5732a862
tanyakuragina <kuragina_ta@mail.ru>
2020-08-13 23:21:52 +0300

    well colored navbar

--- src/components/NaviBar/index.js
+++ src/components/NaviBar/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Navbar, Nav, Form, Button, Row } from 'react-bootstrap';
 import Login from '../Login';
 import { FormGroup } from 'reactstrap';
+import './navibar.css'
 
 export default function NaviBar() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar variant="dark" expand="lg" id='navibar'>
         <Navbar.Brand href="#home">CodeRacer</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
