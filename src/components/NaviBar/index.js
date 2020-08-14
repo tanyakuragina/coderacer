5beefb33f8f7bc7fe7cac79c44d33813938a9559
tanyakuragina <kuragina_ta@mail.ru>
2020-08-14 09:25:45 +0300

    Guest Page release: need to be fixed

--- src/components/NaviBar/index.js
+++ src/components/NaviBar/index.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { Navbar, Nav, Form, Button, Row } from 'react-bootstrap';
 import Login from '../Login';
 import { FormGroup } from 'reactstrap';
-import './navibar.css'
+import './navibar.css';
 
 export default function NaviBar() {
   return (
     <>
-      <Navbar variant="dark" expand="lg" id='navibar'>
-        <Navbar.Brand href="#home">CodeRacer</Navbar.Brand>
+      <Navbar variant="dark" expand="lg" id="navibar">
+        <Navbar.Brand id="codeTitle" href="#home">
+          CodeRacer
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto"></Nav>
-            <Form inline>
-              <Login />
-            </Form>
+          <Form inline>
+            <Login />
+          </Form>
         </Navbar.Collapse>
       </Navbar>
     </>
