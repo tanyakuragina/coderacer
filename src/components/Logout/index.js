fb89ac76641edf73945ab66544a8e2b03a0fe7cf
AbrakhmanovT <abdrakhmanov.timur93@gmail.com>
2020-08-14 12:02:49 +0300

    fixed logout

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, Link, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { FormGroup, Form, Button } from 'reactstrap';
 
 import logoutRequest from '../../redux/thunks/logout.js';
 
