fca8b349c6d75ae0ef4ef6518ff73383662216fe
tanyakuragina <kuragina_ta@mail.ru>
2020-08-14 16:27:33 +0300

    signup fixed 2.0

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -83,50 +83,54 @@ function SignUp() {
                 <MDBCol md="6" xl="5" className="mb-4">
                   {/* <MDBAnimation type="fadeInRight" delay=".3s"> */}
                   <div id="signUpAnimation" type="fadeInRight" delay=".3s">
-                      <div className="card">
-                        <MDBCardBody className="white-text">
-                          <h3 className="text-center" id="registration">
-                            <MDBIcon icon="user" /> Регистрация:
-                          </h3>
-                          <hr className="hr-light" />
-                          <Form
-                            style={{ width: '300px', margin: '0 auto' }}
-                            onSubmit={(e) => {
-                              handleSubmit(e);
-                            }}
-                          >
-                            <FormGroup>
-                              {/* <Label for="username">Ваш ник</Label> */}
-                              <Input
-                                type="text"
-                                name="username"
-                                placeholder="Новый ник"
-                              />
-                            </FormGroup>
-                            <FormGroup>
-                              {/* <Label for="email">Email</Label> */}
-                              <Input
-                                type="text"
-                                name="email"
-                                placeholder="Новый email"
-                              />
-                            </FormGroup>
-                            <FormGroup>
-                              {/* <Label for="password">Password</Label> */}
-                              <Input
-                                type="password"
-                                name="password"
-                                placeholder="Новый пароль"
-                              />
-                            </FormGroup>
-                            <FormGroup>
-                              <Button color="primary" type="submit">
-                                Зарегистрироваться
-                              </Button>
-                            </FormGroup>
-                          </Form>
-                        </MDBCardBody>
-                      </div>
+                    <div className="card">
+                      <MDBCardBody className="white-text m-3">
+                        <h3 className="text-center" id="registration">
+                          <MDBIcon icon="user" /> Регистрация:
+                        </h3>
+                        <hr className="hr-light" />
+                        <Form
+                          style={{
+                            width: '300px',
+                            margin: '0 auto',
+                            height: '300px',
+                          }}
+                          onSubmit={(e) => {
+                            handleSubmit(e);
+                          }}
+                        >
+                          <FormGroup>
+                            {/* <Label for="username">Ваш ник</Label> */}
+                            <Input
+                              type="text"
+                              name="username"
+                              placeholder="Новый ник"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            {/* <Label for="email">Email</Label> */}
+                            <Input
+                              type="text"
+                              name="email"
+                              placeholder="Новый email"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            {/* <Label for="password">Password</Label> */}
+                            <Input
+                              type="password"
+                              name="password"
+                              placeholder="Новый пароль"
+                            />
+                          </FormGroup>
+                          <FormGroup>
+                            <Button color="primary" type="submit">
+                              Зарегистрироваться
+                            </Button>
+                          </FormGroup>
+                        </Form>
+                      </MDBCardBody>
+                    </div>
                   </div>
                   {/* </MDBAnimation> */}
                 </MDBCol>
