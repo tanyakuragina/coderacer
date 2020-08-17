a69bd45a20d010801bc8046733ec6cf481b238d5
tanyakuragina <kuragina_ta@mail.ru>
2020-08-17 08:57:14 +0300

    monday morning

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,13 +7,35 @@ import {
   Link,
   Redirect,
 } from 'react-router-dom';
+import './home.css';
 
 export default function Home() {
   return (
-    <div className="background_home">
-      <h2 id="chui-color">ЧУИ МЫ ДОМА</h2>
-      <Link to="/game">Войти в игру</Link>
-      <UserStatsList />
-    </div>
+    <>
+      <div id='homepage'>
+        <div>
+          <ul>
+            <li>
+              <a href="default.asp">Профайл</a>
+            </li>
+            <li>
+              <a href="news.asp">Создать игру</a>
+            </li>
+            <li>
+              <a href="contact.asp">Турнирная таблица</a>
+            </li>
+            <li>
+              <a href="about.asp">Тренировочный режим</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="background_home">
+         
+          <Link to="/game">Войти в игру</Link>
+          <UserStatsList />
+        </div>
+      </div>
+    </>
   );
 }
