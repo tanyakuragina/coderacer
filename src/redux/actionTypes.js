4050350d243051ebef50bc5e8d713c1538441fde
AbrakhmanovT <abdrakhmanov.timur93@gmail.com>
2020-08-17 09:59:45 +0300

    fixed actioncreators

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -4,4 +4,4 @@ export const sendError = 'SEND_ERROR';
 export const setChallenges = 'SET_CHALLENGES';
 export const setChallenge = 'SET_CHALLENGE';
 export const setGame = 'SET_GAME';
-export const dropGame = 'QUIT_GAME';
+export const dropGame = 'DROP_GAME';
