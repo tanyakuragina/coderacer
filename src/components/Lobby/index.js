fda7e8db4a286f2b9a0871b3f5ba98300e9c6ab0
tanyakuragina <kuragina_ta@mail.ru>
2020-08-18 10:00:13 +0300

    изменила фон на главной + таблица в лобби

--- src/components/Lobby/index.js
+++ src/components/Lobby/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import getOneGame from '../../redux/thunks/getOneGame';
 import { useParams } from 'react-router-dom';
+import { Button, Table } from 'react-bootstrap';
+import '../Lobby/lobby.css';
 
 function Lobby() {
   const game = useSelector((state) => state.game);
@@ -16,12 +18,32 @@ function Lobby() {
 
   return (
     <>
-      <div>OK</div>
-      <div>{game?.author}</div>
-      <div>{game?.startDate}</div>
-      {game?.players.map((el) => (
-        <div>{el.player.username}</div>
-      ))}
+      <div className="lobby">
+        <div className="lobby_shadow">
+          <h1>Игра скоро начнется</h1>
+          <p>здесь должен быть таймер</p>
+          <Table className="w-25" striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Автор игры</th>
+                <th>Время начала игры</th>
+                <th>Игроки</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{game?.author.username}</td>
+                <td>{game?.startDate}</td>
+                <td>
+                  {game?.players.map((el) => (
+                    <div>{el.player.username}</div>
+                  ))}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
     </>
   );
 }
